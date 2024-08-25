7826b3261617b3161a2b8c38364a89b807e99860
LucasBorges2001 <lucasbggv2001@gmail.com>
2024-08-24 22:12:34 -0300

    Creo que estoy al día pero me fijo

--- evento.js
+++ evento.js
@@ -1,3 +1,8 @@
 document.getElementById('contenedorDiv').addEventListener('click', function(){
     alert('Hola! Soy el div');
 })
+
+document.getElementById('btn').addEventListener('click', function(evento) {
+    evento.stopPropagation();
+    alert('Hola!');
+});
